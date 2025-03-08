df5b5e7edf667f7df95d953c246f85bc62343ddb
Neeraj Dalal <admin@nrjdalal.com>
2025-03-08 15:33:42 +0530

    chore: small tweaks 🔥 app/page.tsx, components/auth.tsx 🔥 2 files changed 62+ 62-
    app/page.tsx
    components/auth.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,22 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
 export default async function Home() {
-    const session = await auth.api.getSession({
-        headers: await headers(),
-    });
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-    return (
-        <main className="flex flex-col gap-3 items-center justify-center p-10">
-            {!session ? (
-                <div className="flex gap-3">
-                    <SignIn />
-                    <SignUp />
-                </div>
-            ) : (
-                <div className="flex flex-col gap-3">
-                    <SignOut />
-                </div>
-            )}
-        </main>
-    );
+  return (
+    <main className="flex flex-col gap-3 items-center justify-center p-10">
+      {!session ? (
+        <div className="flex gap-3">
+          <SignIn />
+          <SignUp />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-3">
+          <SignOut />
+        </div>
+      )}
+    </main>
+  );
 }
--- components/auth.tsx
+++ components/auth.tsx
@@ -4,58 +4,58 @@ import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 
 export const SignIn = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <button
-            className="bg-neutral-700 text-white p-2 rounded-md"
-            onClick={async () => {
-                await authClient.signIn.email({
-                    email: "orcdev@test.com",
-                    password: "password123",
-                    // callbackURL: "/dashboard" -> enable to redirect to dashboard
-                });
-                router.refresh();
-            }}
-        >
-            Sign in
-        </button>
-    );
+  return (
+    <button
+      className="bg-neutral-700 text-white p-2 rounded-md"
+      onClick={async () => {
+        await authClient.signIn.email({
+          email: "orcdev@test.com",
+          password: "password123",
+          // callbackURL: "/dashboard" -> enable to redirect to dashboard
+        });
+        router.refresh();
+      }}
+    >
+      Sign in
+    </button>
+  );
 };
 
 export const SignUp = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <button
-            className="bg-neutral-700 text-white p-2 rounded-md"
-            onClick={async () => {
-                await authClient.signUp.email({
-                    email: "orcdev@test.com",
-                    password: "password123",
-                    name: "Orc Dev",
-                    // callbackURL: "/dashboard" -> enable to redirect to dashboard
-                });
-                router.refresh();
-            }}
-        >
-            Sign up
-        </button>
-    );
+  return (
+    <button
+      className="bg-neutral-700 text-white p-2 rounded-md"
+      onClick={async () => {
+        await authClient.signUp.email({
+          email: "orcdev@test.com",
+          password: "password123",
+          name: "Orc Dev",
+          // callbackURL: "/dashboard" -> enable to redirect to dashboard
+        });
+        router.refresh();
+      }}
+    >
+      Sign up
+    </button>
+  );
 };
 
 export const SignOut = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <button
-            className="bg-neutral-700 text-white p-2 rounded-md"
-            onClick={async () => {
-                await authClient.signOut();
-                router.refresh();
-            }}
-        >
-            Sign out
-        </button>
-    );
+  return (
+    <button
+      className="bg-neutral-700 text-white p-2 rounded-md"
+      onClick={async () => {
+        await authClient.signOut();
+        router.refresh();
+      }}
+    >
+      Sign out
+    </button>
+  );
 };
