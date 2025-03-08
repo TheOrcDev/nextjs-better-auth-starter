87cc1135eec05a942dca41080967b0566be34df8
Neeraj Dalal <admin@nrjdalal.com>
2025-03-08 15:26:49 +0530

    chore: small tweaks 🔥 app/page.tsx, app/signout.tsx, components/auth.... 🔥 3 files changed 77+ 37-
    app/page.tsx
    app/signout.tsx
    components/auth.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,24 @@
-import { auth } from "@/lib/auth";
-import { signIn, signUp } from "@/server/users";
-import { headers } from "next/headers";
-import SignOut from "./signout";
+import { SignIn, SignOut, SignUp } from "@/components/auth"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
 
-export default async function Home() {
+export default async function ServerComponent() {
   const session = await auth.api.getSession({
     headers: await headers(),
-  });
+  })
 
   return (
     <main className="flex flex-col gap-3 items-center justify-center p-10">
-      <div className="flex gap-3">
-        <button
-          className="bg-neutral-700 text-white p-2 rounded-md"
-          onClick={signIn}
-        >
-          Sign In
-        </button>
-        <button
-          className="bg-neutral-700 text-white p-2 rounded-md"
-          onClick={signUp}
-        >
-          Sign Up
-        </button>
-        <SignOut />
-      </div>
-      <p>{!session ? "Not authenticated" : session.user.name}</p>
+      {!session ? (
+        <div className="flex gap-3">
+          <SignIn />
+          <SignUp />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-3">
+          <SignOut />
+        </div>
+      )}
     </main>
-  );
+  )
 }
---
+++ components/auth.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { authClient } from "@/lib/auth-client"
+import { useRouter } from "next/navigation"
+
+export const SignIn = () => {
+  const router = useRouter()
+
+  return (
+    <button
+      className="bg-neutral-700 text-white p-2 rounded-md"
+      onClick={async () => {
+        await authClient.signIn.email({
+          email: "johndoe@acme.com",
+          password: "JohnDoeAcmeCom",
+          // callbackURL: "/dashboard" -> enable to redirect to dashboard
+        })
+        router.refresh()
+      }}
+    >
+      Sign in
+    </button>
+  )
+}
+
+export const SignUp = () => {
+  const router = useRouter()
+
+  return (
+    <button
+      className="bg-neutral-700 text-white p-2 rounded-md"
+      onClick={async () => {
+        await authClient.signUp.email({
+          name: "John Doe",
+          email: "johndoe@acme.com",
+          password: "JohnDoeAcmeCom",
+          // callbackURL: "/dashboard" -> enable to redirect to dashboard
+        })
+        router.refresh()
+      }}
+    >
+      Sign up
+    </button>
+  )
+}
+
+export const SignOut = () => {
+  const router = useRouter()
+
+  return (
+    <button
+      className="bg-neutral-700 text-white p-2 rounded-md"
+      onClick={async () => {
+        await authClient.signOut()
+        router.refresh()
+      }}
+    >
+      Sign out
+    </button>
+  )
+}
